9eae3cfeb843ea70d3a75268c002dabe5a8152d6
Arijit Mukherjee <85192566+arijit-muk02@users.noreply.github.com>
2023-04-19 18:05:06 +0530

    Update Calender.js

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,6 +1,7 @@
 import React from "react";
 import myImage2 from '../assets/routine2023.jpg';
 import '../assets/calender.css'
+import { Link } from 'react-router-dom'
 export default function Calender() {
   const handlePrint = () => {
     window.print();
@@ -11,8 +12,8 @@ export default function Calender() {
         <img style={{border:"1px solid black"}} src={myImage2} alt="" className="img-fluid calender_img" />
       </div>
       <div className="text-center">
-      <a href="/about" className="mt-3 btn btn-primary">Back</a>
-      <a href="" className="btn btn-primary mt-3 mx-3" onClick={handlePrint}>Print</a>
+      <Link to="/about" className="mt-3 btn btn-primary">Back</Link>
+      <Link to="" className="btn btn-primary mt-3 mx-3" onClick={handlePrint}>Print</Link>
       </div>
     </div>
   );
