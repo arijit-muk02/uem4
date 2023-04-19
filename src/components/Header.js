20057ffda3becc3a96ad79847e81337c1017452a
Arijit Mukherjee <85192566+arijit-muk02@users.noreply.github.com>
2023-04-19 18:10:21 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../assets/header.css";
-
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div style={{ backgroundColor: "#1C3879" }}>
       <div className="container">
         <div id="header" style={{ padding: "5px" }}>
-          <h1 className="collname"><a style={{color:"white"}} href="/">UNIVERSITY OF ENGINEERING AND MANAGEMENT</a></h1>
+          <h1 className="collname"><Link style={{color:"white"}} to="/">UNIVERSITY OF ENGINEERING AND MANAGEMENT</Link></h1>
           <h3 className="address">
             University Area, Plot No. III, New Town Rd, Kolkata, West Bengal 700160
           </h3>
